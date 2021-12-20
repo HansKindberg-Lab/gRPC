5fee91fb6d3679d08cf7f218e94261ae6c8631b0
HansKindberg <hans.kindberg@gmail.com>
2021-12-20 12:06:46 +0100

    Missed usings

--- Source/Client/Models/Clients/ListDictionaryClient.cs
+++ Source/Client/Models/Clients/ListDictionaryClient.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
 using Microsoft.Extensions.Configuration;
+using Service;
 
 namespace Client.Models.Clients
 {
